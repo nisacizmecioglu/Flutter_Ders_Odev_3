e3ea00c4b8b0b0755b4d722f937daf6f8e2fb0f1
nisacizmecioglu <89413673+nisacizmecioglu@users.noreply.github.com>
2023-11-06 21:58:23 +0300

    Add files via upload

---
+++ app_localizations_en.dart
@@ -0,0 +1,24 @@
+import 'app_localizations.dart';
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get baslik1 => 'Raising funds for';
+
+  @override
+  String get baslik2 => 'Wild life';
+
+  @override
+  String get aciklama => 'We are hoping to get donations in order to fund our projects focused on wild animal rescue and welfare.';
+
+  @override
+  String get toplam => 'Collected';
+
+  @override
+  String get bagisYap => 'Donate Now';
+
+  @override
+  String get paylas => 'Share';
+}
---
+++ app_localizations_tr.dart
@@ -0,0 +1,24 @@
+import 'app_localizations.dart';
+
+/// The translations for Turkish (`tr`).
+class AppLocalizationsTr extends AppLocalizations {
+  AppLocalizationsTr([String locale = 'tr']) : super(locale);
+
+  @override
+  String get baslik1 => 'Bağış toplanıyor';
+
+  @override
+  String get baslik2 => 'Yaban hayatı için';
+
+  @override
+  String get aciklama => 'Vahşi hayvanların kurtarılması ve refahına odaklanan projelerimize fon sağlamak için bağış almayı umuyoruz.';
+
+  @override
+  String get toplam => 'Toplanan';
+
+  @override
+  String get bagisYap => 'Şimdi Bağış Yap';
+
+  @override
+  String get paylas => 'Paylaş';
+}
